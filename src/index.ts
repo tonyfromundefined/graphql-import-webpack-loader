c23c731355964f185bdb66aafb39b52628567e79
Tony Won <jeehyukwon@gmail.com>
2019-08-23 14:42:23 +0900

    add dependencies to webpack and fix bugs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import { importSchema } from 'graphql-import'
+import { importSchema, parseSDL } from 'graphql-import'
+import { resolve, dirname } from 'path'
+import { readFileSync } from 'fs'
 
-export default function(source) {
-  const callback = this.async();
-
+export default function(source: string) {
+  const callback = this.async()
   this.cacheable()
 
-  callback(null, `module.exports = \`${importSchema(source).replace(/`/g, '\\`')}\``)
+  addDependency(this.addDependency, source, this.resourcePath)
+
+  callback(null, `module.exports = \`${importSchema(this.resourcePath).replace(/`/g, '\\`')}\``)
+}
+
+function addDependency(webpackAddDependency: any, source: string, sourcePath: string) {
+  const modules = parseSDL(source)
+
+  for (const module of modules) {
+    const moduleSourcePath = resolve(dirname(sourcePath), module.from)
+    const moduleSource = readFileSync(moduleSourcePath, 'utf-8')
+  
+    webpackAddDependency(moduleSourcePath)
+  
+    addDependency(webpackAddDependency, moduleSource, moduleSourcePath)  
+  }
 }
